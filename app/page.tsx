679089cd15829f15b49b0915fb90badfce0c5893
evmaperry <evmaperry@gmail.com>
2026-05-23 10:29:39 -0400

    first ppost

--- app/page.tsx
+++ app/page.tsx
@@ -1,63 +1,74 @@
-import Image from "next/image";
+import Image from 'next/image';
+import * as React from "react"
+import { Card, CardContent } from "@/components/ui/card"
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel"
 
 export default function Home() {
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
+    <div className='flex flex-col flex-1 items-center justify-center '>
+      <main className='flex flex-1 w-full max-w-3xl flex-col items-center justify-start gap-y-4 py-16 px-16 sm:items-start'>
+        <header className={'mb-6'}>
+          <div className={'text-4xl'}>Tim&apos;s Journey</div>
+        </header>
+
+        <div className={'entry-post'}>
+          <div className={'entry-title-container'}>
+            <div className='entry-title-text'>A new chapter</div>
+            <div className='entry-subtitle-text'>May 23rd</div>
+          </div>
+
+          <div className={'entry-body'}>
+            <div>Hello friends and family,</div>
+            <div>
+              In early May, a series of events unfolded that have led to a new
+              phase of care for Tim. He is at home and comfortable, but there
+              have been significant changes to his health.
+            </div>
+            <div>
+              About two weeks ago, Tim suffered a brain bleed after a fall. He
+              was admitted to Munson and underwent emergency surgery, which
+              stopped the bleeding; however, Tim didn&apos;t recover from his
+              surgery as we hoped he would. Before his fall he was showing
+              symptoms of stage 4 Parkinson&apos;s. His injury has significantly
+              exacerbated those symptoms. As of writing, he recognizes us and
+              can speak a little, but otherwise is very sleepy and requires
+              around-the-clock care.
+            </div>
+            <div>
+              After consulting with medical professionals, we&apos;ve decided to
+              adopt a palliative care approach with Tim. We brought Tim home on
+              May 22nd and are working with Munson Hospice on an in-home basis.
+              This means we will give him comfort care, but not seek further
+              medical intervention.
+            </div>
+            <div>
+              Tim was relieved to get home. It will be wonderful for him to
+              sleep and wake up in his own bed. We are thrilled to have him
+              home. We are hoping that with better sleep, home-cooked meals and
+              boat-loads of love his health can improve to some degree.
+            </div>
+            <div>
+              Thank you so, so much for all of the ongoing love and support
+              you&apos;ve shown us. We&apos;ll update this space periodically to
+              let you know how things are going ❤️
+            </div>
+            <div>
+              With love,
+              <br />
+              the Perry&apos;s
+            </div>
+            <Carousel>
+              <CarouselContent>
+                
+              </CarouselContent>
+            </Carousel>
+          </div>
         </div>
       </main>
     </div>
